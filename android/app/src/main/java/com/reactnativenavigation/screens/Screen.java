7a898446ce0a8986925476ef9ba06b59e4867dbb
Guy Carmeli <guyc@wix.com>
2017-02-24 16:59:31 +0200

    Don’t update screen style on ViewPagerScreenChangedEvent

--- android/app/src/main/java/com/reactnativenavigation/screens/Screen.java
+++ android/app/src/main/java/com/reactnativenavigation/screens/Screen.java
@@ -64,7 +64,6 @@ public abstract class Screen extends RelativeLayout implements Subscriber {
             topBar.onContextualMenuHidden();
         }
         if (ViewPagerScreenChangedEvent.TYPE.equals(event.getType()) && isShown() ) {
-            setStyle();
             topBar.dismissContextualMenu();
         }
     }
