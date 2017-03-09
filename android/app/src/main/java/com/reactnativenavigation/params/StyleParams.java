f85a8dfcd8349e83b58bc2d6af0366ea3906c805
Guy Carmeli <guyc@wix.com>
2017-03-09 11:34:23 +0200

    Save titleBarHidden in screenParams when toggling TitleBar

--- android/app/src/main/java/com/reactnativenavigation/params/StyleParams.java
+++ android/app/src/main/java/com/reactnativenavigation/params/StyleParams.java
@@ -49,7 +49,6 @@ public class StyleParams {
     public Color topBarColor;
     public CollapsingTopBarParams collapsingTopBarParams;
     public boolean topBarCollapseOnScroll;
-    public boolean topBarHidden;
     public boolean topBarElevationShadowEnabled;
     public boolean topTabsHidden;
     public boolean drawScreenBelowTopBar;
