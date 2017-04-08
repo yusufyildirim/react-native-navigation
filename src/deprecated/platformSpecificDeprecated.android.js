206b97dace863973cfe5b4423ab7770672511a85
Guy Carmeli <guyc@wix.com>
2017-04-08 09:31:24 +0300

    Parse lightbox backgroundColor from style

--- src/deprecated/platformSpecificDeprecated.android.js
+++ src/deprecated/platformSpecificDeprecated.android.js
@@ -383,7 +383,7 @@ function showLightBox(params) {
   const backgroundBlur = _.get(params, 'style.backgroundBlur');
   const backgroundColor = _.get(params, 'style.backgroundColor');
   if (backgroundColor) {
-    params.backgroundColor = processColor(params.backgroundColor);
+    params.backgroundColor = processColor(backgroundColor);
   } else {
     if (backgroundBlur === 'dark') {
       params.backgroundColor = processColor('rgba(0, 0, 0, 0.5)');
